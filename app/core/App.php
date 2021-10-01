e85854fb67dfce3d7e64f6ab12d67ab2619fd1e6
FreetyIndrianiSafitri <171821nov>
2021-10-01 12:27:59 -0800

    Menyelesaikan video 3 tentang Routing Model View Controler (MVC)

--- app/core/App.php
+++ app/core/App.php
@@ -1,11 +1,22 @@
 <?php 
+/* Freety Indriani Safitri - 193040175 (MVC)*/
 
 class App {
   public function __construct()
   {
-    echo 'OK!';
+    $url = $this->parseURL();
+    var_dump($url);
   }
 
+  public function parseURL()
+  {
+    if( isset($_GET['url'])){
+      $url = rtrim($_GET['url'], '/');
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+      return $url;
+    }
+  }
 }
 
 ?>
